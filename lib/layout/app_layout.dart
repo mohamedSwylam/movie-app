f9624a57c5d8c37243105a0d4cba598395764054
User Namegit config --global user.name User <mohamedswylam9@gmail.com>
2021-12-26 17:12:45 +0200

    pageVieww

--- lib/layout/app_layout.dart
+++ lib/layout/app_layout.dart
@@ -2,6 +2,7 @@ import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter_offline/flutter_offline.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:movie_app/models/genre.dart';
 import 'package:movie_app/models/movie.dart';
@@ -193,6 +194,14 @@ class AppLayout extends StatelessWidget {
                                 ),
                               ),
                             ),
+                            body: TabBarView(
+                            controller: _tabController,
+              physics: BouncingScrollPhysics(),
+              children: cubit.genre.genres.map((genre) {
+              return GenreMovies(genreId: genre.id,);
+              }).toList(),
+              ),
+              ),
                           ),
                         )),
                   ],
@@ -235,6 +244,119 @@ Widget buildNoInternetWidget() {
             ),
           ),
           Image.asset('assets/images/no_internet.png')
+        ],
+      ),
+    ),
+  );
+}
+
+Widget buildMovieItem(Results model) {
+  return Padding(
+    padding: EdgeInsets.only(top: 10.0, bottom: 10.0, right: 15.0),
+    child: GestureDetector(
+      onTap: () {},
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          model.posterPath == null
+              ? Hero(
+            tag: model.id,
+            child: Container(
+              width: 120.0,
+              height: 180.0,
+              decoration: new BoxDecoration(
+                color: secondColor,
+                borderRadius:
+                BorderRadius.all(Radius.circular(2.0)),
+                shape: BoxShape.rectangle,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    EvaIcons.filmOutline,
+                    color: Colors.white,
+                    size: 60.0,
+                  )
+                ],
+              ),
+            ),
+          )
+              : Hero(
+            tag: model.id,
+            child: Container(
+                width: 120.0,
+                height: 180.0,
+                decoration: new BoxDecoration(
+                  borderRadius:
+                  BorderRadius.all(Radius.circular(2.0)),
+                  shape: BoxShape.rectangle,
+                  image: new DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          "https://image.tmdb.org/t/p/w200/" +
+                              model.posterPath)),
+                )),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            width: 100,
+            child: Text(
+              model.title,
+              maxLines: 2,
+              style: TextStyle(
+                  height: 1.4,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 11.0),
+            ),
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                model.voteAverage.toString(),
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 10.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                width: 5.0,
+              ),
+              RatingBar(
+                ratingWidget: RatingWidget(
+                  empty: Icon(
+                    EvaIcons.star,
+                    color: secondColor,
+                  ),
+                  full: Icon(
+                    EvaIcons.star,
+                    color: secondColor,
+                  ),
+                  half: Icon(
+                    EvaIcons.star,
+                    color: secondColor,
+                  ),
+                ),
+                itemSize: 8.0,
+                initialRating: model.voteAverage / 2,
+                minRating: 1,
+                direction: Axis.horizontal,
+                allowHalfRating: true,
+                itemCount: 5,
+                itemPadding: EdgeInsets.symmetric(horizontal: 2.0),
+                onRatingUpdate: (rating) {
+                  print(rating);
+                },
+              )
+            ],
+          )
         ],
       ),
     ),
