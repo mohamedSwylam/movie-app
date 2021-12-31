4c8009d8823f0b094a8b76efdf555800e1546264
User Namegit config --global user.name User <mohamedswylam9@gmail.com>
2021-12-31 20:47:03 +0200

    api

--- lib/layout/app_layout.dart
+++ lib/layout/app_layout.dart
@@ -20,7 +20,7 @@ class AppLayout extends StatelessWidget {
       listener: (context, state) {},
       builder: (context, state) {
         PageController pageController = PageController(viewportFraction: 1, keepPage: true);
-        var cubit =AppCubit.get(context);
+        var cubit = AppCubit.get(context);
         TabController _tabController;
         return Scaffold(
           backgroundColor: Colors.cyan,
@@ -194,15 +194,127 @@ class AppLayout extends StatelessWidget {
                                 ),
                               ),
                             ),
-                            body: TabBarView(
-                            controller: _tabController,
-              physics: BouncingScrollPhysics(),
-              children: cubit.genre.genres.map((genre) {
-              return GenreMovies(genreId: genre.id,);
-              }).toList(),
-              ),
-              ),
-                          ),
+                            body: Container(
+                              height: 270.0,
+                              padding: EdgeInsets.only(left: 10.0),
+                              child: ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                itemCount: cubit.playingNowMovies.results.length,
+                                itemBuilder: (context, index) {
+                                  return Padding(
+                                    padding: EdgeInsets.only(top: 10.0, bottom: 10.0, right: 15.0),
+                                    child: GestureDetector(
+                                      onTap: () {},
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: <Widget>[
+                                          cubit.playingNowMovies.results[index].poster == null
+                                              ? Hero(
+                                            tag: cubit.playingNowMovies.results[index].id,
+                                            child: Container(
+                                              width: 120.0,
+                                              height: 180.0,
+                                              decoration: new BoxDecoration(
+                                                color: secondColor,
+                                                borderRadius:
+                                                BorderRadius.all(Radius.circular(2.0)),
+                                                shape: BoxShape.rectangle,
+                                              ),
+                                              child: Column(
+                                                mainAxisAlignment: MainAxisAlignment.center,
+                                                children: <Widget>[
+                                                  Icon(
+                                                    EvaIcons.filmOutline,
+                                                    color: Colors.white,
+                                                    size: 60.0,
+                                                  )
+                                                ],
+                                              ),
+                                            ),
+                                          )
+                                              : Hero(
+                                            tag: cubit.playingNowMovies.results[index].id,
+                                            child: Container(
+                                                width: 120.0,
+                                                height: 180.0,
+                                                decoration: new BoxDecoration(
+                                                  borderRadius:
+                                                  BorderRadius.all(Radius.circular(2.0)),
+                                                  shape: BoxShape.rectangle,
+                                                  image: new DecorationImage(
+                                                      fit: BoxFit.cover,
+                                                      image: NetworkImage(
+                                                          "https://image.tmdb.org/t/p/w200/" +
+                                                              cubit.playingNowMovies.results[index].poster)),
+                                                )),
+                                          ),
+                                          SizedBox(
+                                            height: 10.0,
+                                          ),
+                                          Container(
+                                            width: 100,
+                                            child: Text(
+                                              cubit.playingNowMovies.results[index].title,
+                                              maxLines: 2,
+                                              style: TextStyle(
+                                                  height: 1.4,
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 11.0),
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            height: 5.0,
+                                          ),
+                                          Row(
+                                            crossAxisAlignment: CrossAxisAlignment.center,
+                                            children: <Widget>[
+                                              Text(
+                                                cubit.playingNowMovies.results[index]..toString(),
+                                                style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 10.0,
+                                                    fontWeight: FontWeight.bold),
+                                              ),
+                                              SizedBox(
+                                                width: 5.0,
+                                              ),
+                                              RatingBar(
+                                                ratingWidget: RatingWidget(
+                                                  empty: Icon(
+                                                    EvaIcons.star,
+                                                    color: secondColor,
+                                                  ),
+                                                  full: Icon(
+                                                    EvaIcons.star,
+                                                    color: secondColor,
+                                                  ),
+                                                  half: Icon(
+                                                    EvaIcons.star,
+                                                    color: secondColor,
+                                                  ),
+                                                ),
+                                                itemSize: 8.0,
+                                                initialRating: movies[index].rating / 2,
+                                                minRating: 1,
+                                                direction: Axis.horizontal,
+                                                allowHalfRating: true,
+                                                itemCount: 5,
+                                                itemPadding: EdgeInsets.symmetric(horizontal: 2.0),
+                                                onRatingUpdate: (rating) {
+                                                  print(rating);
+                                                },
+                                              )
+                                            ],
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                            ),
                         )),
                   ],
                 );
